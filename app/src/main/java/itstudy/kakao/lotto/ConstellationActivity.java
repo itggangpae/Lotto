cafe2b58dc6f56bff5efeb56a9552d52a61edf23
itggangpae <ggangpae1@gmail.com>
2021-03-26 08:11:57 +0900

    Initial commit

---
+++ app/src/main/java/itstudy/kakao/lotto/ConstellationActivity.java
@@ -0,0 +1,14 @@
+package itstudy.kakao.lotto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConstellationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constellation);
+    }
+}
